0d4da3756608a351dffd1d894c6cb15ce78dad8e
david-skocic-nvent <david.skocic@nvent.com>
2024-09-23 09:38:51 -0400

    archive and clear should be working

--- unzip_and_move.py
+++ unzip_and_move.py
@@ -18,7 +18,9 @@ def archive_and_clear_temp(whichfile: str):
     new_path_to_zip = constants.DATA_ARCHIVE_FOLDER + '\\' + zip_filenames[whichfile]
     # try to move the file and catch any errors
     try:
+        print(f"trying to move {old_path_to_zip} to {new_path_to_zip}...")
         os.rename(old_path_to_zip, new_path_to_zip)
+        print(f"{old_path_to_zip} has been moved to {new_path_to_zip} successfully.")
     except FileNotFoundError:
         print(f"{old_path_to_zip} not found.")
     except FileExistsError:
@@ -26,9 +28,18 @@ def archive_and_clear_temp(whichfile: str):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+    print(f"removing files from {constants.TEMP_DATA_FOLDER}...")
+    # iterate over all files in the path and remove them
     for filename in os.listdir(constants.TEMP_DATA_FOLDER):
-        file_path = os.path.join(constants.TEMP_DATA_FOLDER, filename)
-        print(file_path)
+        filepath = os.path.join(constants.TEMP_DATA_FOLDER, filename)
+        try:
+            print(f"trying to remove {filepath}...")
+            os.remove(filepath)
+            print(f"{filepath} removed successfully.")
+        except FileNotFoundError:
+            print(f"{filepath} not found.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 
 '''
