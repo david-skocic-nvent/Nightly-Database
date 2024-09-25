bc5922000536c3fffe0bdb82db232bd429014eca
david-skocic-nvent <david.skocic@nvent.com>
2024-09-25 08:51:28 -0400

    Code to check if schema is different

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from unzip_and_move import archive_and_clear_temp, unzip_to_temp
 from parse_xml import *
 
-unzip_to_temp('erico')
-tables = get_xml_tables('articles')
-for table in tables:
-    print(table)
-    print("\t" + str(get_varchar_lengths(tables[table])))
+different_files = ['articles', 'units', 'structuregroups', 'structurefeatures', 'products']
+brands = ['caddy', 'erico']
+for b in brands:
+    unzip_to_temp(b)
+    for f in different_files:
+        tables = get_xml_tables(f)
+        for table in tables:
+            print(table)
+            print("\t" + str(get_varchar_lengths(tables[table])))
