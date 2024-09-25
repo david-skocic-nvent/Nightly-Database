b4a9d9d939cb9fc421af5abf022fb4b52ac4c42b
david-skocic-nvent <david.skocic@nvent.com>
2024-09-25 09:22:12 -0400

    checking if all tables for caddy are in erico

--- main.py
+++ main.py
@@ -3,10 +3,31 @@ from parse_xml import *
 
 different_files = ['articles', 'units', 'structuregroups', 'structurefeatures', 'products']
 brands = ['caddy', 'erico']
+tables_list = []
 for b in brands:
     unzip_to_temp(b)
     for f in different_files:
-        tables = get_xml_tables(f)
-        for table in tables:
-            print(table)
-            print("\t" + str(get_varchar_lengths(tables[table])))
+        tables_list.append(get_xml_tables(f))
+
+keysets:list[set] = []
+for tables in tables_list:
+    keysets.append(set(tables.keys()))
+
+print("\n\ndifference::")
+print(keysets[0].difference(keysets[1]))
+print(keysets[1].difference(keysets[0]))
+'''
+for caddy_tables, erico_tables in zip(*tables_list):
+    for table in caddy_tables:
+        if table not in erico_tables:
+            print("MISSING A TABLE-------------\n\n")
+        else:
+            varchar_len_caddy = get_varchar_lengths(caddy_tables[table])
+            varchar_len_erico = get_varchar_lengths(erico_tables[table])
+            for table_len in varchar_len_caddy:
+                if table_len not in varchar_len_erico:
+                    print(f"MISSING COLUMN {table_len} IN TABLE")
+                else:
+                    
+                print("\t" + str(get_varchar_lengths(tables[table])))
+'''
